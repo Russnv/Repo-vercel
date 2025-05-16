0b29d1a9a81d9041cd26896d9b8972e8fbb3a9d5
russnv <russ.natalia.villalba@outlook.com.ar>
2025-05-16 17:10:31 -0300

    datasource

--- back/src/config/dataSource.ts
+++ back/src/config/dataSource.ts
@@ -7,12 +7,24 @@ import { Category } from "../entities/Category";
 import { Product } from "../entities/Product";
 
 export const AppDataSource = new DataSource({
-  type: "postgres",
+  /*type: "postgres",
   host: DB_HOST,
   port: DB_PORT,
   username: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
+  synchronize: true,
+  // dropSchema: true,
+  logging: false,
+  entities: [User, Credential, Order, Product, Category],
+  subscribers: [],
+  migrations: [],*/
+  type: "postgres",
+  host: "dpg-d0jp4d7fte5s7382mp4g-a",
+  port: 5432,
+  username: "dbmitienda_user",
+  password: "zUFy67YkuOhbX1Sa6IXFYX7kK5aaCe7w",
+  database: "dbmitienda",
   synchronize: true,
   // dropSchema: true,
   logging: false,
