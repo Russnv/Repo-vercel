94a83f640fcbbeb5b441587ca6be972fba718b47
russnv <russ.natalia.villalba@outlook.com.ar>
2025-05-16 21:05:33 -0300

    Details

--- front/mitienda/src/app/productos/[id]/Details.tsx
+++ front/mitienda/src/app/productos/[id]/Details.tsx
@@ -6,7 +6,9 @@ import { fetchProductById} from "../../services/Product";
 
 //export default async function Details({ params }: { params: { id: string } }) {
 //export default async function Details({ params }: Props) {
-export default async function Details({ params }: { params: Promise<{ id: string }> }) {
+//export default async function Details({ params }: { params: Promise<{ id: string }> }) {
+export default async function Details({ params }: { params: { id: string } }) {
+
   const { id } = await params;                      
 
   const products = await fetchProductById(id);
